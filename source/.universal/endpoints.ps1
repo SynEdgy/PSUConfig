b58b70116a748413e32b54f9190209b9da67bf02
Gael Colas <gaelcolas@synedgy.com>
2025-11-16 15:14:46 +0100

    demo module not loading by name

--- source/.universal/endpoints.ps1
+++ source/.universal/endpoints.ps1
@@ -1,6 +1,9 @@
 ﻿
-$ModuleFile = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'PSUConfig.psd1'
-Import-Module synedgy.universal.helper
-Import-Module -Name $ModuleFile
+Import-Module -Name synedgy.universal.helper,PSUConfig
+
+
+# $ModuleFile = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'PSUConfig.psd1'
+# Import-Module synedgy.universal.helper
+# Import-Module -Name $ModuleFile
 #TODO: fix Authentication parameter when auth is implemented
 Import-PSUEndpoint -Module PSUConfig -Environment "PSUConfig" -ApiPrefix 'api' -Authentication:$false
