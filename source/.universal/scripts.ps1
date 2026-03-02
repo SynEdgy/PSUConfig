e3db6885e5379a307ee6f3cc2af798ac1142a69f
Gael Colas <gaelcolas@synedgy.com>
2026-03-02 21:42:53 +0100

    adding test of module function without custom attribute

--- source/.universal/scripts.ps1
+++ source/.universal/scripts.ps1
@@ -1,5 +1,7 @@
 ﻿# New-PSUScript -Name "Get-SomeScript.ps1" -Description "Get-SomeScript.ps1" -Path "scripts\Get-SomeScript.ps1" -Environment "Integrated"
+New-PSUScript -Environment "PSUConfig" -Module 'PSUConfig' -Command 'Set-myUser'
 New-PSUScript -Environment "PSUConfig" -Module 'PSUConfig' -Command 'Initialize-PSUConfigDB'
 New-PSUScript -Environment "PSUConfig" -Module 'PSUConfig' -Command 'New-myUser'
 New-PSUScript -Environment "PSUConfig" -Module 'PSUConfig' -Command 'Get-myUser'
+
 New-PSUScript -Environment "PSUConfig" -Path 'scripts\Get-Process.ps1' -Description 'Get-Process.ps1' -Name 'Get-Process.ps1'
