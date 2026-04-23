8e3999db71f8deb74b766f230ee9a938787a9872
Gael Colas <gaelcolas@synedgy.com>
2026-04-23 17:49:06 +0200

    fixing the config always trying to readd the PSResourceRepository

--- source/.universal/settings.ps1
+++ source/.universal/settings.ps1
@@ -1,2 +1,4 @@
-Register-PSResourceRepository -Name output -Uri C:\src\PSUConfig\output -Trusted
+if ($null -eq (Get-PSResourceRepository -Name output)) {
+    Register-PSResourceRepository -Name output -Uri C:\src\PSUConfig\output -Trusted
+}
 Set-PSUSetting -PasswordExpirationDays 360
