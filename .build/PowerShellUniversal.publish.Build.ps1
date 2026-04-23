d92adcbec97564e4072fdea02957e5564761ebc6
Gael Colas <gaelcolas@synedgy.com>
2026-04-23 17:47:16 +0200

    fixing the deploy task

--- .build/PowerShellUniversal.publish.Build.ps1
+++ .build/PowerShellUniversal.publish.Build.ps1
@@ -71,7 +71,8 @@ task publish_packed_module_to_universal_server {
     # $response = Invoke-WebRequest -Uri "$universalServerUrl/api/v1/deployment?asModule=true" -Headers @{
     #     "Authorization" = "Bearer $UniversalServerAppToken"
     # } -InFile $builtNupkgPath -Method Put -ContentType "application/octet-stream"
-    $endpointUrl = "$UniversalServerUrl/api/v1/deployment/module/$ProjectName/$ModuleVersion?repository=output&synchronous=true"
+    # $endpointUrl = "$UniversalServerUrl/api/v1/deployment/module/$ProjectName/$ModuleVersion?repository=output&synchronous=true"
+    $endpointUrl = "$UniversalServerUrl/api/v1/deployment/module/$ProjectName/$($ModuleVersion)?repository=output&synchronous=true"
     $response = Invoke-WebRequest -Uri $endpointUrl -Headers @{
         "Authorization" = "Bearer $UniversalServerAppToken"
     } -InFile $builtNupkgPath -Method Put -ContentType "application/octet-stream"
